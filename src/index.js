ad399222e878c7af6f5f76bac05d12381ee60752
jamescross <crossjames813@gmail.com>
2021-08-15 14:13:05 -0500

    attempt

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 let addToy = false;
 
+
+function fetchToys(){
+  fetch("http://localhost:3000/toys")
+  .then(resp => console.log(resp))
+  }
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  // let toyBin = document.getElementById('toy-collection')
+  
+ 
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
